f1f40129c41214b90fd4351e36628e01dab1d695
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-02 21:06:53 +0100

    added resume

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ const App = () => {
     <div className="bg-gray-100 min-h-screen relative">
       <div className="ui-circle absolute top-16 left-13 opacity-20 animate-pulse"></div>
       <div className="ui-circle absolute left-5 opacity-20 animate-pulse"></div>
-      
-     
 
       <Hero />
       <Projects />
       <footer className="text-black text-center py-4 mt-8">
+        <a
+          href="./src/assets/LinhNguyen.pdf"
+          download="Linh_Nguyen_Resume.pdf"
+          className="text-pink-900 text-2xl font-semibold hover:underline"
+        >
+          Download Resume
+        </a>
         <p>&copy; 2025 Linh Nguyen. All rights reserved.</p>
+        
       </footer>
     </div>
   );
