6d75bd62062450d8ab15beb4f9c5835a2e3786a8
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-02 20:23:37 +0100

    ui-circle

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Projects from "./Projects";
 const App = () => {
   return (
     <div className="bg-gray-100 min-h-screen relative">
-      <div className="ui-circle absolute top-0 left-0 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute top-20 right-20 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute left-10 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute right-5 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-16 left-13 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute left-5 opacity-20 animate-pulse"></div>
+      
+     
+
       <Hero />
       <Projects />
       <footer className="text-black text-center py-4 mt-8">
