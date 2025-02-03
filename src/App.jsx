48f14becd02d2622e24dec16b7c9063a30c1b4a6
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-03 02:50:15 +0100

    more circles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import Projects from "./Projects";
 const App = () => {
   return (
     <div className="bg-gray-100 min-h-screen relative">
-      <div className="ui-circle absolute top-16 left-13 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute left-5 opacity-20 animate-pulse"></div>
-
+      <div className="ui-circle absolute top-5 left-10 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-10 right-16 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-20 left-1/3 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-24 right-1/4 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-12 left-1/4 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-16 right-1/3 opacity-20 animate-pulse"></div>
+      <div className="ui-circle absolute top-28 left-1/2 opacity-20 animate-pulse"></div>
       <Hero />
       <Projects />
       <footer className="text-black text-center py-4 mt-8">
@@ -19,7 +23,6 @@ const App = () => {
           Download Resume
         </a>
         <p>&copy; 2025 Linh Nguyen. All rights reserved.</p>
-        
       </footer>
     </div>
   );
