c65e8ffaa2483e6a79b8f1db982d7b6728b9f395
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-06 04:55:04 +0100

    Add new kitties image and update Hero and App components
    - Added a new image of kitties to the public directory
    - Updated the Hero component to include the new image
    - Modified the App component to improve layout and styling

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,19 +3,31 @@ import {
   FaGithub,
   FaLinkedin,
 } from "react-icons/fa";
+import { motion } from "framer-motion";
+
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center text-black min-h-screen text-center">
+    <section className="relative flex flex-col items-center justify-center text-black min-h-screen text-center">
+    
+      <motion.div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('./kitties.png')" }}
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0.5 }}
+        transition={{ duration: 3, ease: "easeOut" }}
+      ></motion.div>
+      
+      <div className="absolute inset-0 bg-gradient-to-b from-white via-transparent to-white"></div>
+      
       <h1 className="text-5xl font-bold mb-4">linh nguyen</h1>
-
       <div className="max-w-3xl px-4">
         <p className="text-lg mb-8">
           Hi, I&apos;m a project manager with lots of energy and a sense of
-          humor. At night, I turn into a software developer, building
-          fun apps that makes me happy. Currently exploring cloud computing because... why not 👽!
-          I love swimming in the ocean, Vietnamese street food, and my cats, 
-          along with all things art and all things code.
+          humor. At night, I turn into a software developer, building fun apps
+          that makes me happy. Currently exploring cloud computing because...
+          why not 👽! I love swimming in the ocean, Vietnamese street food, and
+          my cats, along with all things art and all things code.
         </p>
       </div>
       <div className="flex space-x-6 mt-4 mb-6">
