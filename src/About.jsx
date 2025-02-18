362d2a94eb676c942f10d364e6aa613dc9373d0c
Linh Nguyen <jakuzzi263@gmail.com>
2025-02-18 21:58:41 +0100

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="container mx-auto px-6 py-12 bg-transparent">
           <h1 className="text-3xl text-black text-center mb-12 flex items-center justify-center gap-2 flex-wrap">
-           all about me
+           all about me 🙋🏻‍♀️
           </h1>
 
           <Tabs />
