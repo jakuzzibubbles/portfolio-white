a886806112f0ab3824e23ab7f819f1df6d084da7
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-18 22:02:13 +0100

    update about

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="container mx-auto px-6 py-12 bg-transparent">
           <h1 className="text-3xl text-black text-center mb-12 flex items-center justify-center gap-2 flex-wrap">
-           all about me 🙋🏻‍♀️
+           all about me
           </h1>
 
           <Tabs />
