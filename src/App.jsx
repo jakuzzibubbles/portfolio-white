c2cf038bb4827501b96e9d16562fa44830206969
jakuzzibubbles <jakuzzi263@gmail.com>
2025-02-19 07:59:50 +0100

    removed unused functions

--- src/App.jsx
+++ src/App.jsx
@@ -31,22 +31,10 @@ function App() {
 
   return (
     <div className="bg-white min-h-screen relative">
-      {/* <div className="ui-circle absolute top-64 left-5 sm:left-10 md:left-20 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute top-80 left-16 sm:left-32 opacity-20 animate-pulse"></div>
-      <div className="ui-circle absolute top-96 left-24 sm:left-48 opacity-20 animate-pulse"></div> */}
-
       <Hero />
       <Projects />
       <About />
       <footer className="text-black text-center py-4 mt-8">
-        {/* <a
-          href="./Linh Nguyen Resume.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white text-2xl bg-slate-700 border-2 px-4 rounded-lg transition duration-300 hover:bg-slate-200/80 hover:text-slate-900"
-        >
-          view resume
-        </a> */}
         <p className="mt-8">&copy; 2025 linh nguyen</p>
       </footer>
     </div>
